df0d8ef5b47a8480889243b7aa4bb2db54b2da96
gpapov <gpapov@stream.ru>
2020-10-21 11:44:54 +0300

    add filter in message form

--- src/main/java/com/example/likeweater/repos/MessageRepo.java
+++ src/main/java/com/example/likeweater/repos/MessageRepo.java
@@ -3,5 +3,8 @@ package com.example.likeweater.repos;
 import com.example.likeweater.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
 }
